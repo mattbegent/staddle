0f4e259f3d63ff6aa340124438bd6b606ea42e19
Matt Begent <matt@mattbegent.co.uk>
2012-07-12 09:17:22 +0100

    Common js libs added, structure for main.js

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/* ==========================================================================
+// Main.js
+// Reference: http://viget.com/inspire/extending-paul-irishs-comprehensive-dom-ready-execution
+// =========================================================================*/
+
+SITENAME = {
+
+
+    /* ===================== Common ========================*/
+    common: {
+
+        init: function() {
+            //site-wide code
+
+            //Signal js is present
+            $("html").removeClass("no-js");
+
+    },
+
+
+    /* ===================== home ========================*/
+    home: {
+
+        init: function() {
+            // controller-wide code
+        },
+
+        show: function() {
+            // action-specific code
+        }
+
+    }
+
+};
+
+UTIL = {
+    exec: function( controller, action ) {
+        var ns = SITENAME,
+        action = ( action === undefined ) ? "init" : action;
+
+        if ( controller !== "" && ns[controller] && typeof ns[controller][action] == "function" ) {ns[controller][action]();}
+    },
+
+    init: function() {
+        var body = document.body,
+        controller = body.getAttribute( "data-controller" ),
+        action = body.getAttribute( "data-action" );
+
+        UTIL.exec( "common" );
+        UTIL.exec( controller );
+        UTIL.exec( controller, action );
+    }
+};
+
+$( document ).ready( UTIL.init );
